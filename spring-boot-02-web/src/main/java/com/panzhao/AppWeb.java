85742b9b5216ddbb4a825c25cd7a4e5f6eaf3fb2
Administrator <747011882@qq.com>
2020-12-29 16:52:50 +0800

    jdbc

---
+++ spring-boot-02-web/src/main/java/com/panzhao/AppWeb.java
@@ -0,0 +1,14 @@
+package com.panzhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot 启动类
+ */
+@SpringBootApplication
+public class AppWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(AppWeb.class, args);
+    }
+}
---
+++ spring-boot-02-web/src/main/java/com/panzhao/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.panzhao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
